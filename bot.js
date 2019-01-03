0d4e73595b28fb8dba9d7e6cf65894ed9f2704fc
Iceman013 <45405964+Iceman013@users.noreply.github.com>
2019-01-03 17:33:09 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+
+var Discord = require('discord.io');
+var logger = require('winston');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: process.env.token,
+   autorun: true
+});
+bot.on('ready',function(evt){
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
