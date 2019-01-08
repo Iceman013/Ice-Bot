7dabde464e2d8ac3e865796513915c3031fafdda
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2019-01-07 20:05:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,10 +18,11 @@ bot.on('ready',function(evt){
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', function (user, userID, channelID, message, evt)){
     if (message == '$test'){
         bot.sendMessage({
             to: channelID,
-            message: 'Test successful.'})
+            message: 'Test successful.'
+        })
     }
 }
