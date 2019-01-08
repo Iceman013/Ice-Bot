be8bbdbb6c72ff8ff2c60cd955783909d05d8bab
Iceman013 <45405964+Iceman013@users.noreply.github.com>
2019-01-07 19:48:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,3 +18,10 @@ bot.on('ready',function(evt){
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+bot.on('message', function (user, userID, channelID, message, evt) {
+    if (message == '$test'){
+        bot.sendMessage({
+            to: channelID,
+            message: 'Test successful.'})
+    }
+}
