33f040f0d7b2a5929e589085dd90d945477ac65f
Iceman013 <45405964+Iceman013@users.noreply.github.com>
2019-01-14 21:01:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -488,7 +488,7 @@ bot.on('message', function (user, userID, channelID, message, evt){
                 if (evt.d.mentions[1].id != undefined){
                     b = evt.d.mentions[0].username
                     c = evt.d.mentions[1].username
-                    output = ':hearts:**' + b + ' :revolving_hearts: ' + c + ' = ' + b.substring(0, Math.floor(b.length)) + c.substring(Math.floor(c.length), c.length) + '**:hearts:'
+                    output = ':hearts:**' + b + ' :revolving_hearts: ' + c + ' = ' + b.substring(0, Math.floor(0.5*b.length)) + c.substring(Math.floor(0.5*c.length), c.length) + '**:hearts:'
                 }
             }
                     
