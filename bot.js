f643b902e8ea74366f1498fd0376d4b5cd895a43
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2019-03-18 20:51:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,7 +83,7 @@ while (b < eightBallA.length && b < eightBallB.length && b < eightBallC.length){
     }
 }
 
-bot.on('messageUpdate', function(oldMsgData, newMsgData, evt){
+bot.on('messageUpdate', function(oldMsgData, newMsgData, evt)){
     var message = newMsgData.content
     console.log(newMsgData)
     console.log(message)
