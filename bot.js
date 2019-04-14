5c5f2604c31cae3085407a496fd1b372dff5462c
Iceman013 <45405964+Iceman013@users.noreply.github.com>
2019-04-13 21:56:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -281,6 +281,7 @@ bot.on('message', function (user, userID, channelID, message, evt){
 			c = c + g[b]
 			b = b + 1
 		}
+		console.log(c)
 		bot.deleteMessage({
 			channelID: channelID,
 			messageID: c})
